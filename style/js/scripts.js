f128a53a29251f3c2e32024d2ed29fc29d0c9e85
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2020-06-16 19:27:08 +0200

    prove search

--- style/js/scripts.js
+++ style/js/scripts.js
@@ -1,5 +1,61 @@
 $(document).ready(function() {
 	'use strict';
+
+	/*-----------------------------------------------------------------------------------*/
+	/*	Hardvark SEARCH
+	/*-----------------------------------------------------------------------------------*/
+
+	$('body')
+	  .on('click', 'div.three button.btn-search', function(event) {
+	    event.preventDefault();
+	    var $input = $('div.three input');
+	    $input.focus();
+	    if ($input.val().length() > 0) {
+	      // submit form
+	    }
+	  })
+	  .on('click', 'div.four button.btn-search', function(event) {
+	    event.preventDefault();
+	    var $input = $('div.four input');
+	    $input.focus();
+	    if ($input.val().length() > 0) {
+	      // submit form
+	    }
+	  })
+	  .on('click', 'div.five button.btn-search', function(event) {
+	    event.preventDefault();
+	    var $input = $('div.five input');
+	    $input.focus();
+	    if ($input.val().length() > 0) {
+	      // submit form
+	    }
+	  })
+	  .on('click', 'div.six button.btn-search', function(event) {
+	    event.preventDefault();
+	    var $input = $('div.six input');
+	    $input.focus();
+	    if ($input.val().length() > 0) {
+	      // submit form
+	    }
+	  })
+	  .on('click', 'div.thirteen button.btn-search', function(event) {
+	    event.preventDefault();
+	    var $input = $('div.thirteen input');
+	    $input.focus();
+	    if ($input.val().length() > 0) {
+	      // submit form
+	    }
+	  })
+	  .on('click', 'div.fourteen button.btn-search', function(event) {
+	    event.preventDefault();
+	    var $input = $('div.fourteen input');
+	    $input.focus();
+	    if ($input.val().length() > 0) {
+	      // submit form
+	    }
+	  })
+	;
+
 	/*-----------------------------------------------------------------------------------*/
 	/*	STICKY HEADER
 	/*-----------------------------------------------------------------------------------*/
@@ -167,7 +223,7 @@ $(document).ready(function() {
 	      $grid.isotope({ filter: filterValue });
 	    });
 	  });
-	    
+
 	  $('.isotope-filter').each( function( i, buttonGroup ) {
 	    var $buttonGroup = $( buttonGroup );
 	    $buttonGroup.on( 'click', '.button', function() {
@@ -175,7 +231,7 @@ $(document).ready(function() {
 	      $( this ).addClass('active');
 	    });
 	  });
-	
+
 	};
 	enableIsotope();
 	/*-----------------------------------------------------------------------------------*/
